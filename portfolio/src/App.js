53050e25c66399383702215d5e3f95433d71c4be
poojashelke8 <poojabshelke0807@gmail.com>
2024-04-17 14:48:00 +0530

    1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Body  = styled.div`
+background-color: ${({theme}) => theme.bg};
+width:100%;
+overflow-x:hidden;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello There</h1>
     </div>
   );
 }
