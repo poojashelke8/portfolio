b953eba33fc263e2bece6f41330ebdd71505836f
poojashelke8 <poojabshelke0807@gmail.com>
2024-04-25 18:07:25 +0530

    3

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,7 +4,9 @@ import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from './utils/Themes';
 import HeroSection from './Components/HeroSection/HeroSection';
 import Skills from './Components/Skills/Skills';
-import Navbar from './Components/Navbar/navbar';
+import Navbar from './Components/Navbar/Navbar';
+import Education from './Components/Education/Education';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Body  = styled.div`
 background-color: ${({theme}) => theme.bg};
@@ -13,15 +15,26 @@ height:100px;
 overflow-x:hidden;
 `;
 
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
+      <Router>
+
       <Navbar/>
       <Body>
-        portfolio
         <HeroSection/>
+        <Wrapper>
         <Skills/>
+        <Education/>
+        </Wrapper>
       </Body>
+      </Router>
     </ThemeProvider>
   );
 }
--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from "../Navbar/NavbarStyled"
+// import {NavLink} from "react-router-dom";
+// import styled from "styled-components"
+// import { Link as LinkR } from 'react-router-dom';
+// import { DiCssdeck } from 'react-icons/di';
+// import { FaBars } from 'react-icons/fa';
+// import { Bio } from '../../data/constants';
+// import { Close, CloseRounded } from '@mui/icons-material';
+// import { useTheme } from 'styled-components';
+
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <Nav>
+      <NavbarContainer>
+        <NavLogo>Logo</NavLogo>
+        <MobileIcon></MobileIcon>
+        <NavItems>
+          <NavLink href='#about'>About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#projects'>Projects</NavLink>
+          <NavLink href='#education'>Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton>Github Profile</GitHubButton>
+        </ButtonContainer>
+      </NavbarContainer>
+    </Nav>
   )
 }
 
