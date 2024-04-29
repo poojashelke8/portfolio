7949eb75222df45e407ba3126a76deb269c13cb4
poojashelke8 <poojabshelke0807@gmail.com>
2024-04-29 12:32:45 +0530

    4.01

--- portfolio/src/Components/HeroSection/HeroSection.js
+++ portfolio/src/Components/HeroSection/HeroSection.js
@@ -4,18 +4,18 @@ import HeroBgAnimation from '../HeroBgAnimation/HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, SocialMediaIcons, SocialMediaIcon, ResumeButton } from "../HeroSection/HeroStyled"
 import { Bio } from '../../data/constants'
 import TypeWriter from "typewriter-effect"
-import HeroImg from '../../Images/HeroImage.jpg'
+//import HeroImg from '../../Images/HeroImage.jpg'
 
 const HeroSection = () => {
   return (
     <div id="about">
       <HeroContainer>
       <HeroBg>
-                    <HeroBgAnimation />
-                </HeroBg>
-        <HeroInnerContainer>
+        <HeroBgAnimation />
+      </HeroBg>
+      <HeroInnerContainer>
           <HeroLeftContainer>
-            <Title>Hi,I am <br/>{Bio.name}</Title>
+            <Title>Hi, I am <br/>{Bio.name}</Title>
             <TextLoop>
               I am a 
               <span>
@@ -32,7 +32,7 @@ const HeroSection = () => {
             <ResumeButton href={Bio.resume} target="display">Check Resume</ResumeButton>
           </HeroLeftContainer> 
           <HeroRightContainer>
-          <Img src={HeroImg} alt="hero-image" />
+          {/* <Img src={HeroImg} alt="hero-image" /> */}
           </HeroRightContainer>
         </HeroInnerContainer>
       </HeroContainer>
