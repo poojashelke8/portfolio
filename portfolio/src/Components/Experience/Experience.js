46e44fad2ab14b07fbef207e0f1fe4baed0027ed
poojashelke8 <poojabshelke0807@gmail.com>
2024-05-02 22:35:08 +0530

    6-experience

--- portfolio/src/Components/Experience/Experience.js
+++ portfolio/src/Components/Experience/Experience.js
@@ -6,7 +6,7 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-// import ExperienceCard from '../Cards/ExperienceCard';
+import ExperienceCard from '../Cards/ExperienceCard';
 import { experiences } from '../../data/constants';
 
 const Container = styled.div`
@@ -89,10 +89,11 @@ const Experience = () => {
                                 {/* Hello */}
                                 <TimelineSeparator>
                                     <TimelineDot variant="outlined" color="secondary" />
-                                    {index !== experiences.length - 1 && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                    {index !== experiences.length - 1 && 
+                                    <TimelineConnector style={{ background: '#854CE6' }} />}
                                 </TimelineSeparator>
                                 <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                    {/* <ExperienceCard experience={experience}/> */}
+                                    <ExperienceCard experience={experience}/>
                                 </TimelineContent>
                             </TimelineItem>
                         ))}
